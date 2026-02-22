fc5b5d7bd2b3a98130e9f797028fbeb5b41f29de
Edvinas <edvinasbureika@gmail.com>
2026-02-23 01:17:06 +0200

    array functions

--- 02-arrays-and-iteration/02-array-functions/index.php
+++ 02-arrays-and-iteration/02-array-functions/index.php
@@ -1,5 +1,61 @@
 <?php
 $output = null;
+
+$ids = [1, 22, 3, 4, 67];
+$users = ['user1', 'user6', 'user3'];
+
+$output = count($ids);
+
+// sort
+sort($ids);
+sort($users);
+
+// resver order sort
+rsort($ids);
+rsort($users);
+
+// array_push
+array_push($ids, 100);
+array_push($users, "user100");
+
+// array_pop
+array_pop($ids);
+array_pop($users);
+
+// array_shift
+array_shift($ids);
+array_shift($users);
+
+// array_unshift
+array_unshift($ids, 100);
+array_unshift($users, "user100");
+
+
+// array_slice !!! returns a new array
+$newIds = array_slice($ids, 1, 2);
+// var_dump($newIds);
+
+// array_splice
+array_splice($ids, 0, 2, [100, 200]);
+
+$output = "Sum of ids: " . array_sum($ids);
+
+// array_search
+$output = "User 3 is at index: " . array_search("user3", $users);
+
+// in_array
+$output = "User 3 exists: " . in_array("user3", $users);
+
+// explode: string to arr
+$tags = "php,javascript,html,css";
+$tagsArr = explode(",", $tags);
+
+// var_dump($tagsArr);
+
+// implode: arr to string
+$output = implode(";", $users);
+
+
 ?>
 
 <!DOCTYPE html>
@@ -22,6 +78,17 @@ $output = null;
         <div class="bg-white rounded-lg shadow-md p-6 mt-6">
             <!-- Output -->
             <p class="text-xl"><?= $output ?></p>
+            <h2 class="text-xl font-semibold my-4">IDS array:</h2>
+            <p>
+            <pre>
+                    <?php print_r($ids) ?>
+                </pre>
+            </p>
+            <h2 class="text-xl font-semibold my-4">USERS array:</h2>
+            <p>
+            <pre>
+                    <?php print_r($users) ?>
+                </pre>
             </p>
         </div>
     </div>
