dd6ea60f256ce9cf1c47a4ec6c3092e9df29896b
Edvinas <edvinasbureika@gmail.com>
2026-03-08 01:13:42 +0200

    superglobal

--- 06-superglobals/05-request/index.php
+++ 06-superglobals/05-request/index.php
@@ -1,3 +1,17 @@
+<?php
+
+$title = '';
+$age = '';
+$submitted = false;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_REQUEST['submit'])) {
+  $name = htmlspecialchars($_REQUEST['name']) ?? '';
+  $age = htmlspecialchars($_REQUEST['age']) ?? '';
+  $submitted = true;
+}
+
+?>
+
 <form method="post">
   <div>
     <label for="name">Name:</label>
@@ -9,3 +23,7 @@
   </div>
   <input type="submit" name="submit" value="Submit">
 </form>
+
+<?php if ($submitted) : ?>
+<p>Hello, <?php echo $name; ?>! You are <?php echo $age; ?> years old.</p>
+<?php endif; ?>
