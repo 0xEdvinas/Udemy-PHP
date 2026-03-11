01385721e3d98c9aefb282ac47e1fd4757e895e0
Edvinas <edvinasbureika@gmail.com>
2026-03-11 02:44:27 +0200

    remove post

---
+++ 07-database/11-delete-records/delete.php
@@ -0,0 +1,28 @@
+<?php
+require 'database.php';
+
+function deletePost($id)
+{
+  global $pdo;
+
+  $sql = 'DELETE FROM posts WHERE id = :id';
+
+  $stmt = $pdo->prepare($sql);
+
+  $stmt->execute(['id' => $id]);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && $_POST['_method'] === 'delete' && isset($_POST['id']))
+{
+  $id = $_POST['id'];
+  deletePost($id);
+  header('Location: index.php');
+  exit;
+}
+
+
+// echo '<pre>';
+// var_dump($_POST);
+// echo '</pre>';
+
+?>
